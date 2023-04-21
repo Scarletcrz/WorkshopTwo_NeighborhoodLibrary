b88e591b54ad1c3d2c7b33a684ae090361c9fed8
Scarlet Cruz <scruz@appdev.yearup.org>
2023-04-21 14:06:31 -0400

    added constructors

--- src/main/java/com/sc/Main.java
+++ src/main/java/com/sc/Main.java
@@ -2,6 +2,31 @@ package com.sc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println();
     }
+}
+
+///Properties
+class Book {
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String isCheckedTo;
+
+// Constructors
+
+    public Book( int id, String isbn,String title, boolean isCheckedOut, String isCheckedTo) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = isCheckedOut;
+        this.isCheckedTo= isCheckedTo;
+    }
+
+// Getters and Setters
+    public int id(){
+        return this.id;
+    }
+
 }
